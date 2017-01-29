6fbcc03488f48f2d1c7b9d404b9d48035c67abc8
Nate <negbert91@gmail.com>
2017-01-29 15:03:57 -0500

    took out the if/else statement to shorten the code.

--- exercises/002 - functions and requires/01isEvenlyDivisible.js
+++ exercises/002 - functions and requires/01isEvenlyDivisible.js
@@ -3,15 +3,9 @@ module.exports = {
     //THIS METHOD SHOULD RETURN TRUE WHEN THERE IS NO REMAINDER dividend / divisor
     //EX true: 4 / 2 = 2    there is no remainder in this example
     //EX false: 5 / 2 = 2.5 there is .5 remainder in this example
-    isEvenlyDivisible: function(dividend, divisor)
-    {
+    isEvenlyDivisible: function(dividend, divisor) {
 
-        if(dividend % divisor === 0){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return dividend % divisor === 0;
 
     }
 };
